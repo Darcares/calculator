6825ef1862c903a06a5d61d0dde151cc3093fd52
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-01-17 10:24:21 +0000

    Create function operate()
    The function operate takes 3 arguments.
    Operator, a and b.
    The operator argument select which operation the user want to perform.
    A and b are the numbers that will be operated.

--- script.js
+++ script.js
@@ -11,5 +11,33 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
- return a / b;
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    let result = null;
+
+    switch(operator) {
+
+        case 0:
+            result = add(a, b);
+            break;
+        
+        case 1:
+            result = subtract(a, b);
+            break;
+        
+        case 2:
+            result = multiply(a, b);
+            break;
+
+        case 3:
+            result = divide(a, b);
+            break;
+        
+        default:
+            break;
+    }
+
+    return result;
 }
