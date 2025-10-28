dca508aede340a76e6effc6d4f64e4b7e61cd992
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2025-10-28 03:47:08 +0000

    Code operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numOne = null;
+let numTwo = null;
+let operator = null;
+
 function add(numOne, numTwo) {
     return numOne + numTwo;
 }
@@ -12,4 +16,28 @@ function multiply(numOne, numTwo) {
 
 function divide(numOne, numTwo) {
     return numOne / numTwo;
+}
+
+function operate(numOne, numTwo, operator) {
+    switch(operator) {
+        case "add":
+            return add(numOne,numTwo);
+            break;
+
+        case "subtract":
+            return subtract(numOne, numTwo);
+            break;
+
+        case "multiply":
+            return multiply(numOne, numTwo);
+            break;
+
+        case "divide": 
+            return divide(numOne, numTwo);
+            break;
+
+        default:
+            console.log("Error");
+            break;
+    }
 }
